2366044d851486a17f51d0baebffa0bef3d253eb
ItaiYosephi <itai.yosephi@gmail.com>
2020-10-15 11:09:13 +0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NbThemeModule, NbLayoutModule, NbIconModule, NbIconLibraries} from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { AppRoutingModule } from './app-routing.module';
-import {CustomNbIconLibraries} from './custom-nb-icon-libraries';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbIconModule,
+  NbIconLibraries
+} from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { AppRoutingModule } from "./app-routing.module";
+import { CustomNbIconLibraries } from "./custom-nb-icon-libraries";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot({ name: "default" }),
     NbLayoutModule,
     NbEvaIconsModule,
     AppRoutingModule,
     NbIconModule
   ],
-  // **** Uncomment to Reproduce **** ///
-  providers: [{provide: NbIconLibraries, useExisting: CustomNbIconLibraries}],
+  // **** comment to Reproduce **** ///
+  providers: [{ provide: NbIconLibraries, useExisting: CustomNbIconLibraries }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
@@ -31,10 +34,10 @@ export class AppModule {
   }
 
   private registerMaterialIcons() {
-    this.iconLibraries.registerFontPack('material-icons', {
-      packClass: 'material-icons',
-      ligature: true,
+    this.iconLibraries.registerFontPack("material-icons", {
+      packClass: "material-icons",
+      ligature: true
     });
-    this.iconLibraries.setDefaultPack('material-icons');
+    this.iconLibraries.setDefaultPack("material-icons");
   }
 }
